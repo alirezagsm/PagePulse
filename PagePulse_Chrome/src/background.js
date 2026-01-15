d0e17a991080579e89f7e373adc1b040793dbd4f
alirezagsm <alirezaa.g@gmail.com>
2026-01-15 01:15:50 +0100

    update to using alarms for longer refresh intervals

--- PagePulse_Chrome/src/background.js
+++ PagePulse_Chrome/src/background.js
@@ -6,10 +6,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		const frequency = request.frequency;
 		if (reloadIntervals[tabId]) {
 			clearInterval(reloadIntervals[tabId]);
+			delete reloadIntervals[tabId];
 		}
-		reloadIntervals[tabId] = setInterval(() => {
-			chrome.tabs.reload(tabId);
-		}, frequency * 1000);
+		chrome.alarms.clear(`reload_${tabId}`);
+
+		if (frequency >= 60) {
+			chrome.alarms.create(`reload_${tabId}`, {
+				periodInMinutes: frequency / 60,
+			});
+		} else {
+			reloadIntervals[tabId] = setInterval(() => {
+				chrome.tabs.reload(tabId);
+			}, frequency * 1000);
+		}
+
 		chrome.storage.local.set({
 			[`reloadFrequency_${tabId}`]: frequency,
 			[`isReloading_${tabId}`]: true,
@@ -20,6 +30,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		});
 		sendResponse({ status: "Reload started for tab " + tabId });
 	} else if (request.action === "stopReload") {
+		chrome.alarms.clear(`reload_${tabId}`);
 		if (reloadIntervals[tabId]) {
 			clearInterval(reloadIntervals[tabId]);
 			delete reloadIntervals[tabId];
@@ -32,15 +43,35 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	}
 });
 
+chrome.alarms.onAlarm.addListener((alarm) => {
+	if (alarm.name.startsWith("reload_")) {
+		const tabId = parseInt(alarm.name.split("_")[1], 10);
+		chrome.tabs.reload(tabId, (updatedTab) => {
+			if (chrome.runtime.lastError) {
+				chrome.alarms.clear(alarm.name);
+				chrome.storage.local.set({ [`isReloading_${tabId}`]: false });
+			}
+		});
+	}
+});
+
 chrome.runtime.onStartup.addListener(() => {
 	chrome.storage.local.get(null, (data) => {
 		for (const key in data) {
 			if (key.startsWith("isReloading_") && data[key]) {
 				const tabId = parseInt(key.split("_")[1], 10);
 				const frequency = data[`reloadFrequency_${tabId}`];
-				reloadIntervals[tabId] = setInterval(() => {
-					chrome.tabs.reload(tabId);
-				}, frequency * 1000);
+
+				if (frequency >= 60) {
+					chrome.alarms.create(`reload_${tabId}`, {
+						periodInMinutes: frequency / 60,
+					});
+				} else {
+					reloadIntervals[tabId] = setInterval(() => {
+						chrome.tabs.reload(tabId);
+					}, frequency * 1000);
+				}
+
 				chrome.action.setIcon({
 					path: "../images/reload-active.png",
 					tabId: tabId,
--- PagePulse_Firefox/src/background.js
+++ PagePulse_Firefox/src/background.js
@@ -1,15 +1,10 @@
-let reloadIntervals = {};
-
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	const tabId = request.tabId;
 	if (request.action === "startReload") {
 		const frequency = request.frequency;
-		if (reloadIntervals[tabId]) {
-			clearInterval(reloadIntervals[tabId]);
-		}
-		reloadIntervals[tabId] = setInterval(() => {
-			browser.tabs.reload(tabId);
-		}, frequency * 1000);
+		browser.alarms.create(`reload_${tabId}`, {
+			periodInMinutes: frequency / 60,
+		});
 		browser.storage.local.set({
 			[`reloadFrequency_${tabId}`]: frequency,
 			[`isReloading_${tabId}`]: true,
@@ -20,15 +15,22 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		});
 		sendResponse({ status: "Reload started for tab " + tabId });
 	} else if (request.action === "stopReload") {
-		if (reloadIntervals[tabId]) {
-			clearInterval(reloadIntervals[tabId]);
-			delete reloadIntervals[tabId];
-		}
+		browser.alarms.clear(`reload_${tabId}`);
 		browser.storage.local.set({
 			[`isReloading_${tabId}`]: false,
 		});
 		browser.action.setIcon({ path: "../images/reload.png", tabId: tabId });
 		sendResponse({ status: "Reload stopped for tab " + tabId });
+	}
+});
+
+browser.alarms.onAlarm.addListener((alarm) => {
+	if (alarm.name.startsWith("reload_")) {
+		const tabId = parseInt(alarm.name.split("_")[1], 10);
+		browser.tabs.reload(tabId).catch(() => {
+			browser.alarms.clear(alarm.name);
+			browser.storage.local.set({ [`isReloading_${tabId}`]: false });
+		});
 	}
 });
 
@@ -38,9 +40,9 @@ browser.runtime.onStartup.addListener(() => {
 			if (key.startsWith("isReloading_") && data[key]) {
 				const tabId = parseInt(key.split("_")[1], 10);
 				const frequency = data[`reloadFrequency_${tabId}`];
-				reloadIntervals[tabId] = setInterval(() => {
-					browser.tabs.reload(tabId);
-				}, frequency * 1000);
+				browser.alarms.create(`reload_${tabId}`, {
+					periodInMinutes: frequency / 60,
+				});
 				browser.action.setIcon({
 					path: "../images/reload-active.png",
 					tabId: tabId,
